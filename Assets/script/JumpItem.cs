03bbd16d8e12bc0b2d61592fac4abcfbe64dc98e
DESKTOP-J3CLVL8\vantan <0xc32525105508a@ezweb.ne.jp>
2022-07-09 18:44:53 +0900

    arakimasakazu

---
+++ Assets/script/JumpItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class JumpItem : ItemBase2D
+{
+    /// <summary>ジャンプパワーを増幅させる値</summary>
+    [SerializeField] int Jump = 10;
+
+    public override void Activate()
+    {
+        FindObjectOfType<PlayerController2D>()._jumpSpeed += Jump;
+    }
+}
---
+++ Assets/script/MoveSpeedItem.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MoveSpeedItem : ItemBase2D
+{
+    /// <summary>スピードを増幅させる値</summary>
+    [SerializeField] int moveSpeed = 10;
+
+    public override void Activate()
+    {
+        FindObjectOfType<PlayerController2D>()._moveSpeed += moveSpeed;
+    }
+}
+
